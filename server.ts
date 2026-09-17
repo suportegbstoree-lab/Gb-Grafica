95912731108c934d4226ca5af4f0b4d9ee6cdad5
Belagamba Eduardo <hendo_@tutamail.com>
2026-09-17 07:34:43 -0300

    Fix Express 5 SPA wildcard route

--- server.ts
+++ server.ts
@@ -43,7 +43,7 @@ async function startServer() {
       },
     }));
     
-    app.get('*', (req, res) => {
+    app.get('/{*splat}', (req, res) => {
       res.setHeader('Cache-Control', 'no-cache');
       res.sendFile(path.join(distPath, 'index.html'));
     });
